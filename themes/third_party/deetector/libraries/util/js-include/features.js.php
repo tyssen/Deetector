5e5fc8870ac20c9311aba3d373873b034b1e669c
John Faulds <registrations@tyssendesign.com.au>
2013-04-26 23:02:46 +1000

    Fixed path to Deetector in features.js.php

--- themes/third_party/deetector/libraries/util/js-include/features.js.php
+++ themes/third_party/deetector/libraries/util/js-include/features.js.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
     // to be included in your <head></head> tag to run the per-request tests
 	$p = true; // turn off the build function
-	require("path/to/Detector/Detector.php");
+	require("../../Detector.php");
 	header("content-type: application/x-javascript");
 	Detector::perrequest();
 ?>
